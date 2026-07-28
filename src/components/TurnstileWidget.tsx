64dda096af9b487666d0e51ef2b680eb83362e02
Madaisy Dev <dev@madaisy-consulting.com>
2026-07-28 09:58:16 +0000

    fix: sitekey Turnstile en dur

--- src/components/TurnstileWidget.tsx
+++ src/components/TurnstileWidget.tsx
@@ -4,7 +4,7 @@ import Turnstile from 'react-turnstile';
 export default function TurnstileWidget({ onVerify }: { onVerify: (token: string) => void }) {
   return (
     <Turnstile
-      sitekey={process.env.NEXT_PUBLIC_TURNSTILE_SITE_KEY || '1x00000000000000000000AA'}
+      sitekey={process.env.NEXT_PUBLIC_TURNSTILE_SITE_KEY || "0x4AAAAAAD_23zRui3sA6kp5"}
       onVerify={onVerify}
       theme="light"
     />
